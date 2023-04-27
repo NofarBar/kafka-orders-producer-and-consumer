d35ecea8696e0d4cc1df5ce63bfa6b05edcbfd42
RyanMcklain <nofar.barzakai@gmail.com>
2023-04-27 09:50:29 +0300

    Typos

--- order-manager/src/main/java/com/webserver/ordermanager/OrderManagerApplication.java
+++ order-manager/src/main/java/com/webserver/ordermanager/OrderManagerApplication.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class OrderManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerApplication.class, args);
 	}
-
 }
